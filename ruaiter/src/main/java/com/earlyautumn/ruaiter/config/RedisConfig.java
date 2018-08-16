f6cbc85c8c53283d5dd7857a6f7dfc3a9f8f051e
17 <2540705109@qq.com>
2018-08-16 23:52:15 +0800

    redis not test

--- ruaiter/src/main/java/com/earlyautumn/ruaiter/config/RedisConfig.java
+++ ruaiter/src/main/java/com/earlyautumn/ruaiter/config/RedisConfig.java
@@ -5,34 +5,59 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
-import java.util.Arrays;
+import java.time.Duration;
 
 //@Configuration
 //@EnableCaching
 public class RedisConfig {
 
-//    @Bean
-//    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
-//        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
-//        // 多个缓存的名称,目前只定义了一个
-//        rcm.setCacheNames(Arrays.asList("thisredis"));
-//        //设置缓存过期时间(秒)
-//        rcm.setDefaultExpiration(600);
-//        return rcm;
-//    }
+    /**
+     * 生成key的策略
+     */
+    @Bean
+    public KeyGenerator keyGenerator() {
 
-//    @Bean
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+
+        };
+    }
+
+    /**
+     * 管理缓存
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // 设置缓存有效期一小时
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(1));
+        return RedisCacheManager.builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory)).cacheDefaults(redisCacheConfiguration).build();
+
+    }
+
+    /**
+     * RedisTemplate配置
+     */
+    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
