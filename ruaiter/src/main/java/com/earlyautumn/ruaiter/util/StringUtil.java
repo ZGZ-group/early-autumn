c0a303c776f19350fc8eca5dfefff4a0505a2833
17 <2540705109@qq.com>
2018-09-06 21:24:16 +0800

    util

--- ruaiter/src/main/java/com/earlyautumn/ruaiter/util/StringUtil.java
+++ ruaiter/src/main/java/com/earlyautumn/ruaiter/util/StringUtil.java
@@ -1,24 +1,34 @@
 package com.earlyautumn.ruaiter.util;
 
 public class StringUtil {
-//
-//    public static String trim(String str) {
-//
-//    }
 
-//    public boolean isEmpty(String str) {
-//        if (null == str || str.length() == 0) {
-//            return true;
-//        }
+    private static final String EMPTY_STRING = "";
+    private static final String ONE_SPACE_STRING = " ";
 
-//        char[] chars = str.toCharArray();
-//        for (int i = 0; i < chars.length; i++) {
-//            if (' ' == chars[i]) {
-//
-//            }
-//
-//        }
+    public static String trim(String str) {
+        if (isEmpty(str)) {
+            return EMPTY_STRING;
+        }
 
-//    }
+        return str.replaceAll(ONE_SPACE_STRING, EMPTY_STRING);
+
+    }
+
+    public static boolean isEmpty(String str) {
+        if (null == str || str.length() == 0) {
+            return true;
+        }
+
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (' ' != c) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
 
 }
