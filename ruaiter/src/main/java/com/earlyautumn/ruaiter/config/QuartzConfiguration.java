12fe201c4761ab26be7ba71728b0d62785d40227
17 <2540705109@qq.com>
2018-09-12 22:02:56 +0800

    doc quartz

---
+++ ruaiter/src/main/java/com/earlyautumn/ruaiter/config/QuartzConfiguration.java
@@ -0,0 +1,33 @@
+package com.earlyautumn.ruaiter.config;
+
+import com.earlyautumn.ruaiter.config.job.TestQuartz;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfiguration {
+
+    private static final String JOB_NAME = "testQuartz";
+
+    @Bean
+    public JobDetail jobDetail() {
+        JobBuilder jobBuilder = JobBuilder.newJob(TestQuartz.class);
+        jobBuilder.withIdentity(JOB_NAME);
+        return jobBuilder.storeDurably().build();
+    }
+
+
+    @Bean
+    public Trigger trigger(JobDetail jobDetail) {
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("*/5 * * * * ?");
+
+        TriggerBuilder<Trigger> triggerTriggerBuilder = TriggerBuilder.newTrigger();
+        triggerTriggerBuilder.forJob(jobDetail);
+        triggerTriggerBuilder.withIdentity(JOB_NAME);
+        triggerTriggerBuilder.withSchedule(scheduleBuilder);
+
+        return triggerTriggerBuilder.build();
+    }
+
+}
