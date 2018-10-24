333f3e05a3131f04c3c0fb537c63da53cc178538
zhongliangjian <1097292941@qq.com>
2018-10-24 10:21:20 +0800

    first

--- ruaiter/src/main/java/com/earlyautumn/ruaiter/util/StringUtils.java
+++ ruaiter/src/main/java/com/earlyautumn/ruaiter/util/StringUtils.java
@@ -18,7 +18,7 @@ public class StringUtils {
     }
 
     /**
-     * 2判断字符串是否为空
+     *   判断字符串是否为空
      */
     public static boolean isEmpty(String str) {
         if (null == str || str.length() == 0) {
